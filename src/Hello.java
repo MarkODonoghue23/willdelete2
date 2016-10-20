358a99bd474c1bd2864f20f65339096df03d809f
MarkODonoghue23 <mar>
2016-10-20 10:46:58 +0100

    Initial Commit simple hi message
    No Interest amount calculation
    added code to calculate the interest for a set principle, interest rate and
    Number of years

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hi");
+
+        int numYears = 10;
+        float principal = 10000.0f;
+        float interestRate = 5.0f;
+        float amount = (float)(principal * Math.pow( (1 + (interestRate/100.0f)), numYears));
+        System.out.println(amount);
+
     }
 }
